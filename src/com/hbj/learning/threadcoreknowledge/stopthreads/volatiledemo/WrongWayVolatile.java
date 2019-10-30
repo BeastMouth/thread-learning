98e13221e7dba53a0c00c916352b95ca80012240
hbj <hbj@33.cn>
2019-10-30 14:13:48 +0800

    错误的停止线程的方法：利用volatile关键字判断boolean来停止线程

---
+++ src/com/hbj/learning/threadcoreknowledge/stopthreads/volatiledemo/WrongWayVolatile.java
@@ -0,0 +1,36 @@
+package com.hbj.learning.threadcoreknowledge.stopthreads.volatiledemo;
+
+/**
+ * 演示用volatile的局限：part1 看似可行
+ *
+ * @author hbj
+ * @date 2019/10/30 13:01
+ */
+public class WrongWayVolatile implements Runnable {
+
+    private volatile boolean canceled = false;
+
+    @Override
+    public void run() {
+        int num = 0;
+        try {
+            while (num <= 100000 && !canceled) {
+                if (num % 100 == 0) {
+                    System.out.println(num + "是100的倍数。");
+                }
+                num++;
+                Thread.sleep(1);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        WrongWayVolatile r = new WrongWayVolatile();
+        Thread thread = new Thread(r);
+        thread.start();
+        Thread.sleep(5000);
+        r.canceled = true;
+    }
+}
