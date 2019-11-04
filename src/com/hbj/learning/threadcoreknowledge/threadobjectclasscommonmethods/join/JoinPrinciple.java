2f30a3ee24cc247389eb487e53de3bd1c44b9997
hbj <hbj@33.cn>
2019-11-04 21:14:10 +0800

    Join的等价写法

---
+++ src/com/hbj/learning/threadcoreknowledge/threadobjectclasscommonmethods/join/JoinPrinciple.java
@@ -0,0 +1,29 @@
+package com.hbj.learning.threadcoreknowledge.threadobjectclasscommonmethods.join;
+
+/**
+ * @author hbj
+ * @date 2019/11/4 21:11
+ */
+public class JoinPrinciple {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + "执行完毕");
+            }
+        });
+
+        thread.start();
+        System.out.println("开始等待子线程运行完毕");
+        thread.join();
+//        synchronized (thread) {
+//            thread.wait();
+//        }
+        System.out.println("所有子线程执行完毕");
+    }
+}
