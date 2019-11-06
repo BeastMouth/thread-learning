c1179514471a075592e6478763c6f99caeb0fd8e
hbj <hbj@33.cn>
2019-11-06 23:25:15 +0800

    可见性例子

--- src/com/hbj/learning/jmm/FieldVisibility.java
+++ src/com/hbj/learning/jmm/FieldVisibility.java
@@ -2,6 +2,7 @@ package com.hbj.learning.jmm;
 
 /**
  * 演示可见性带来的问题
+ * b=3;a=1(第二个线程可能看不见或者看到一部分第一个线程的操作)
  *
  * @author hbj
  * @date 2019/11/6 23:08
@@ -12,6 +13,7 @@ public class FieldVisibility {
     volatile int b = 2;
 
     private void change() {
+        // 可能执行一句就刷到主内存
         a = 3;
         b = a;
     }
