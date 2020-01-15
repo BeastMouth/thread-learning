3a50673fd8b9e320fa00af19c6c9725bf96ce86d
hbj <hbj@33.cn>
2020-01-15 17:40:22 +0800

    可见性更新

--- src/com/hbj/learning/jmm/FieldVisibility.java
+++ src/com/hbj/learning/jmm/FieldVisibility.java
@@ -11,6 +11,8 @@ public class FieldVisibility {
 
     volatile int a = 1;
     volatile int b = 2;
+//    int a = 1;
+//    int b = 2;
 
     private void change() {
         // 可能执行一句就刷到主内存
@@ -18,9 +20,19 @@ public class FieldVisibility {
         b = a;
     }
 
+    //    private void print() {
+//        System.out.println("a=" + a + ";b=" + b);
+//    }
     private void print() {
         System.out.println("b=" + b + ";a=" + a);
     }
+
+    // 没有加volatile关键字
+    // a=3,b=3 第一个线程先执行，第二个线程再执行
+    // a=1,b=2 第二个线程先执行，第一个线程在执行
+    // a=3,b=2 两个线程交替运行
+    // a=1,b=3(线程间通讯，线程1只把b刷回了主存，没有把a刷回主存)
+
 
     public static void main(String[] args) {
         while (true) {
